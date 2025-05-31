1749a6c3908699cce6fcc7640de3d719bfa845ae
WEH <1485029705@qq.com>
2025-05-31 13:02:16 +0800

    add close control button to the settings window

--- electron/src/renderer/components/SettingsWindowComponents/RightScreen/RightScreen.jsx
+++ electron/src/renderer/components/SettingsWindowComponents/RightScreen/RightScreen.jsx
@@ -2,32 +2,34 @@ import React, { Children } from 'react';
 import ReactDOM from 'react-dom';
 import './RightScreen.css'
 import WindowControl from '../../../templates/WindowControl/WindowControl.jsx';
-export default function RightScreen({content}){
+import {Button} from 'antd';
+import {CloseOutlined} from '@ant-design/icons';
+export default function RightScreen({content,onClick}){
     switch(content){
         case 'lidongdong':
             return(
-                <RightScreenContainer>
+                <RightScreenContainer onClick = {onClick}>
                     <div>李冬冬</div>
                     {console.log('Li Dongdong has been awaken.')}
                 </RightScreenContainer>
             )
         case 'zhangjing':
             return(
-                <RightScreenContainer>
+                <RightScreenContainer onClick = {onClick}>
                     <div>张静</div>
                     {console.log('Zhang Jing has been awaken.')}
                 </RightScreenContainer>
             )
         case 'guoweibin':
             return(
-                <RightScreenContainer>
+                <RightScreenContainer onClick = {onClick}>
                     <div>郭卫斌</div>
                     {console.log('Guo Weibin has been awaken.')}
                 </RightScreenContainer>
             )
         default:
             return(
-                <RightScreenContainer>
+                <RightScreenContainer onClick = {onClick}>
                     <div>什么也没有</div>
                     <div>什么也没有</div>
                     <div>什么也没有</div>
@@ -77,12 +79,16 @@ export default function RightScreen({content}){
             )
     }
 }
-function RightScreenContainer({children}){
+function RightScreenContainer({children,onClick}){
     return(
         <div className = 'rightscreen-container'>
             {/* <div style = {{display: 'flex', marginLeft: 'auto'}}>
                 <WindowControl></WindowControl>
             </div> */}
+            <div className = 'closebar-container'>
+                <Button icon = {<CloseOutlined></CloseOutlined>} onClick = {onClick}>
+                </Button>
+            </div>
             <div className = 'rightscreen-main'>
                 {children}
             </div>
