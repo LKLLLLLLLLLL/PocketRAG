0423bd6fde95663b426fd227b65248ab3277785c
WEH <1485029705@qq.com>
2025-06-11 20:55:57 +0800

    about us

--- electron/renderer/components/SettingsWindowComponents/About/About.jsx
+++ electron/renderer/components/SettingsWindowComponents/About/About.jsx
@@ -1,23 +1,36 @@
 import React, { useEffect, useState } from 'react';
 import './About.css';
-const About = ({version,onSaveAllSettings}) => {
-    return(        <div className = "about-container">
-            <h1>关于PocketRAG</h1>
-            <div className = 'about-content'>
-                <div className = "content version">
-                    Version: v{version}
-                </div>
-                <div className = "content author">
-                    Author: LKL XZR WEH
-                </div>
-                <div className = "content update">
-                    Update: 2025-6-11
+
+const About = ({version, onSaveAllSettings}) => {
+    // 定义一个函数来打开链接
+    const handleOpenLink = () => {
+        window.open('https://github.com/LKLLLLLLLLLL/PocketRAG', '_blank', 'noopener,noreferrer');
+    };
+
+    return(        
+        <div style={{display: 'flex', height: '100%', width: '100%', flexDirection: 'column'}}>
+            <div className="about-container">
+                <h1>关于PocketRAG</h1>
+                <div className='about-content'>
+                    <div className="content version">
+                        Version: v{version}
+                    </div>
+                    <div className="content author">
+                        Author: LKL XZR WEH
+                    </div>
+                    <div className="content update">
+                        Update: 2025-6-11
+                    </div>
                 </div>
             </div>
-            
-            {/* 底部空白块，防止被悬浮按钮遮挡 */}
-            <div className="bottom-spacer"></div>
+            <div className="link-container" style={{display:'flex', justifyContent: 'center', alignItems: 'center', color:"darkgrey"}}>
+                {/* 使用div和事件来替代a标签 */}
+                <div onClick={handleOpenLink} style={{cursor: 'pointer', cursor: 'hand', color: 'darkgrey'}}>
+                    更多信息点此处访问
+                </div>
+            </div>
         </div>
     )
 }
+
 export default About;
